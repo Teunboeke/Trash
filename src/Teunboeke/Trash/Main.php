a70f95a36dc52cd3e9943cbd1b5b3a0a85868357
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-30 21:43:25 +0100

    Update Main.php

--- src/Teunboeke/Trash/Main.php
+++ src/Teunboeke/Trash/Main.php
@@ -3,10 +3,10 @@
 namespace Teunboeke\Trash;
 
 use pocketmine\block\Block;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
-use pocketmine\command\CommandExecutor;
+use pocketmine\command\Commandexecutor;
+use pocketmine\command\Command;
+use pocketmine\command\Commandsender;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerInteractEvent;
