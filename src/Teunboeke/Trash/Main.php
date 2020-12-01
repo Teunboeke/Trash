38b0f5ab1d0680655edeadd604b744fd0c970405
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-12-01 18:39:39 +0100

    Update Main.php

--- src/Teunboeke/Trash/Main.php
+++ src/Teunboeke/Trash/Main.php
@@ -113,8 +113,8 @@ class Main extends PluginBase implements Listener {
             			$inv->setItem(20, $glass);
             			$inv->setItem(21, $glass);
             			$inv->setItem(22, $glass);
-                  $inv->setitem(23, $glass);
-                  $imv->setitem(23, $glass);
+                    $inv->setitem(23, $glass);
+                    $imv->setitem(23, $glass);
             
             			$inv->setItem(25, $glass);
             			$inv->setItem(26, $exit);
